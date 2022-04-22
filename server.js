7a5359e77629d8c4c3be5c563a6a573917134a21
hernandez-g <g5hernandez@yahoo.com>
2022-04-22 14:59:08 -0700

    middleware for user

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
 var methodOverride = require('method-override');
 
 require('dotenv').config();
 require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,12 +19,25 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.used(methodOverride('_method'));
+
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(function(req, res, next){
+  res.locals.user = req.user;
+  next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
